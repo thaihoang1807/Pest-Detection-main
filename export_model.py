9b38d4d1968b9c32b3857597b76c066788c46f1d
thaihoang1807 <122801356+thaihoang1807@users.noreply.github.com>
2026-07-15 14:53:47 +0700

    feat: deploy real yolo11 model

--- export_model.py
+++ export_model.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # 1. Load file PT của bạn
-model = YOLO(r"F:\thực_tập\Pest-Detection-main\backend\models\version\best_v2_nano.pt")
+model = YOLO(r"F:\thực_tập\Pest-Detection-main\backend\models\version\best_yolo11_final.pt")
 
 # 2. Xuất ra định dạng ONNX
 print("⏳ Đang ép cân sang ONNX...")
